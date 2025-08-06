699fffac9f40fafb73b300e29a40482e0ea8c795
Ahmed Mustafa <am1804067@gmail.com>
2025-08-06 21:52:00 +0300

    Remove unused code in Program.cs

--- OrdrMate/Program.cs
+++ OrdrMate/Program.cs
@@ -1,23 +1,8 @@
-using System.Security.Claims;
-using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.FileProviders;
-using Microsoft.IdentityModel.Tokens;
-using OrdrMate.Data;
 using OrdrMate.Middlewares;
-using OrdrMate.Repositories;
-using OrdrMate.Services;
-using OrdrMate.Managers;
-using OrdrMate.Sockets;
-using Google.Apis.Auth.OAuth2;
 using Hangfire;
-using Hangfire.MemoryStorage;
-using OrdrMate.Configs;
-using MongoDB.Driver;
 using OrdrMate.Core;
-using Microsoft.EntityFrameworkCore;
-using OrdrMate.Utils;
 
 var builder = WebApplication.CreateBuilder(args);
 var env = builder.Environment;
@@ -33,23 +18,6 @@ foreach (var module in modules)
 {
     module.Register(builder.Services, builder.Configuration, env);
 }
-
-// PostgreSQL Database Context
-// builder.Services.AddDbContext<OrdrMateDbContext>(options =>
-//     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// MongoDB Configuration
-// builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection("MongoDb"));
-// builder.Services.AddSingleton<IMongoClient, MongoClient>(sp =>
-// {
-//     var settings = builder.Configuration.GetSection("MongoDb").Get<MongoDbSettings>();
-//     return new MongoClient(settings?.ConnectionString);
-// });
-
-// MongoDB Context
-// builder.Services.AddScoped<OrdrMateMongoContext>();
-
-
 
 // CORS
 builder.Services.AddCors(options =>
@@ -73,89 +41,8 @@ builder.Services.AddCors(options =>
     });
 });
 
-// Repositories and Services
-
-// builder.Services.AddScoped<IUserRepo, UserRepo>();
-// builder.Services.AddScoped<ManagerService, ManagerService>();
-// builder.Services.AddScoped<CustomerService, CustomerService>();
-
-// builder.Services.AddScoped<IRestaurantRepo, RestaurantRepo>();
-// builder.Services.AddScoped<RestaurantService, RestaurantService>();
-
-// builder.Services.AddScoped<IItemRepo, ItemRepo>();
-// builder.Services.AddScoped<ItemService, ItemService>();
-
-// builder.Services.AddScoped<IBranchRepo, BranchRepo>();
-// builder.Services.AddScoped<BranchService, BranchService>();
-// builder.Services.AddScoped<IBranchRequestRepo, BranchRequestRepo>();
-
-// builder.Services.AddScoped<ITableRepo, TableRepo>();
-// builder.Services.AddScoped<TableService, TableService>();
-
-// builder.Services.AddScoped<IKitchenRepo, KitchenRepo>();
-// builder.Services.AddScoped<KitchenService, KitchenService>();
-
-
-// builder.Services.AddScoped<IOrderRepo, OrderRepo>();
-// builder.Services.AddScoped<OrderService, OrderService>();
-// builder.Services.AddScoped<IDeliverRequestRepo, DeliverRequestRepo>();
-// builder.Services.AddScoped<IPaymentRepo, PaymentRepo>();
-// builder.Services.AddScoped<PaymentService, PaymentService>();
-
-// builder.Services.AddScoped<ICustomizationRepo, CustomizationRepo>();
-// builder.Services.AddScoped<CustomizationService, CustomizationService>();
-
-// builder.Services.AddScoped<GeoMaps>();
-
-// builder.Services.AddScoped<CloudMessaging>();
-
-// Sockets
-// builder.Services.AddScoped<BranchSocketHandler>();
-
-// Managers
-// builder.Services.AddScoped<OrderManager>();
-// builder.Services.AddScoped<TableManager>();
-
-// Third-party services
-// builder.Services.AddHttpClient<PaymobService>();
-
-// builder.Services.AddHttpClient<AiService>();
-
-// builder.Services.AddScoped<S3Service>();
-
-// Hangfire
-// builder.Services.AddHangfire(config =>
-// {
-//     config.UseSimpleAssemblyNameTypeSerializer()
-//     .UseRecommendedSerializerSettings()
-//     .UseMemoryStorage();
-// });
-
-// builder.Services.AddHangfireServer();
-
-// builder.Services.AddScoped<IBackgroundJobClient, BackgroundJobClient>();
-
 builder.Services.AddControllers();
 
-// JWT Authentication
-
-// builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-//     .AddJwtBearer(options =>
-//     {
-//         options.TokenValidationParameters = new TokenValidationParameters
-//         {
-//             ValidateIssuer = true,
-//             ValidateAudience = true,
-//             ValidateLifetime = true,
-//             ValidateIssuerSigningKey = true,
-//             ValidIssuer = builder.Configuration["Jwt:Issuer"],
-//             ValidAudience = builder.Configuration["Jwt:Audience"],
-//             IssuerSigningKey = new SymmetricSecurityKey(
-//                 Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)
-//             ),
-//             RoleClaimType = ClaimTypes.Role,
-//         };
-//     });
 
 // Authorization
 builder.Services.AddAuthorization();
@@ -198,4 +85,3 @@ app.UseStaticFiles(new StaticFileOptions
 app.UseHttpsRedirection();
 app.MapControllers();
 app.Run();
-
