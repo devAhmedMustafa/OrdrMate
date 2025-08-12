c1c79b4317ad21ccba0dcc489c37afcea4deb6a3
Ahmed Mustafa <am1804067@gmail.com>
2025-08-12 13:03:49 +0300

    Add customizations to Order item dto

---
+++ OrdrMate/DTOs/Order/OrderItemDto.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using OrdrMate.DTOs.Item;
+
+namespace OrdrMate.DTOs.Order;
+
+public class OrderItemDto
+{
+    public required string ItemId { get; set; }
+    public int Quantity { get; set; } = 1;
+    [Required] public decimal Price { get; set; }
+    public ItemDto? Item { get; set; } = null;
+    public Dictionary<string, object>? Customizations { get; set; } = null;
+}
--- OrdrMate/DTOs/Order/PlaceOrderDto.cs
+++ OrdrMate/DTOs/Order/PlaceOrderDto.cs
@@ -1,16 +1,7 @@
 using System.ComponentModel.DataAnnotations;
-using OrdrMate.DTOs.Item;
 using OrdrMate.Enums;
 
 namespace OrdrMate.DTOs.Order;
-
-public class OrderItemDto
-{
-    public required string ItemId { get; set; }
-    public int Quantity { get; set; } = 1;
-    [Required] public decimal Price { get; set; }
-    public ItemDto? Item { get; set; } = null;
-}
 
 public class PlaceOrderDto
 {
