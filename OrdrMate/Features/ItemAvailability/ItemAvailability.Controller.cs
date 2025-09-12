edc4ff637d3520d6d4cca90aba740e03199c6c3d
Ahmed Mustafa <am1804067@gmail.com>
2025-09-13 02:41:21 +0300

    Add authorization layer

--- OrdrMate/Features/ItemAvailability/ItemAvailability.Controller.cs
+++ OrdrMate/Features/ItemAvailability/ItemAvailability.Controller.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace OrdrMate.Features.ItemAvailability;
@@ -7,17 +8,29 @@ namespace OrdrMate.Features.ItemAvailability;
 public class ItemAvailabilityController : ControllerBase
 {
     private readonly ItemAvailabilityService _itemAvailabilityService;
+    private readonly IAuthorizationService _authorizationService;
 
-    public ItemAvailabilityController(ItemAvailabilityService itemAvailabilityService)
+    public ItemAvailabilityController(
+        ItemAvailabilityService itemAvailabilityService,
+        IAuthorizationService authorizationService
+        )
     {
         _itemAvailabilityService = itemAvailabilityService;
+        _authorizationService = authorizationService;
     }
 
     [HttpPut("toggle")]
+    [Authorize(Roles = "BranchManager")]
     public async Task<IActionResult> ToggleItemAvailability([FromBody] ToggleItemAvailabilityDto dto)
     {
         try
         {
+            var isAuthorized = await _authorizationService.AuthorizeAsync(User, dto.BranchId, "BranchManager");
+            if (!isAuthorized.Succeeded)
+            {
+                return Forbid();
+            }
+
             var isAvailable = await _itemAvailabilityService.ToggleItemAvailability(dto.ItemId, dto.BranchId);
             return Ok(isAvailable);
         }
