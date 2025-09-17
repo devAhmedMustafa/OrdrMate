622b6cc837bf51da2e920e92b307f15deb2c26df
Ahmed Mustafa <am1804067@gmail.com>
2025-09-17 09:08:29 +0300

    Fix identation

--- OrdrMate/Controllers/OrderContoller.cs
+++ OrdrMate/Controllers/OrderContoller.cs
@@ -510,35 +510,28 @@ public class OrderController : ControllerBase
     }
 
     [HttpPost("create-order")]
-[Authorize(Roles = "BranchManager")]
-public async Task<ActionResult<OrderIntentDto>> CreateOrderForBranchManager([FromBody] PlaceOrderDto placeOrderDto)
-{
-    try
+    [Authorize(Roles = "BranchManager")]
+    public async Task<ActionResult<OrderIntentDto>> CreateOrderForBranchManager([FromBody] PlaceOrderDto placeOrderDto)
     {
-        var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-        if (string.IsNullOrEmpty(userId))
-            return Forbid("User ID not found in claims.");
-
-        placeOrderDto.CustomerId = userId;
-
-        var branch = await _branchService.GetBranchById(placeOrderDto.BranchId);
-
-        if (!TimeService.CheckWithinTimeInterval(branch.StartWorkingHour, branch.EndWorkingHour, branch.WorkingDays))
+        try
         {
-            return Forbid("Branch is not open at this time.");
-        }
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (string.IsNullOrEmpty(userId))
+                return Forbid("User ID not found in claims.");
 
-        var orderIntent = await _orderService.CreateOrderIntent(placeOrderDto);
-        if (orderIntent == null)
+            placeOrderDto.CustomerId = userId;
+
+            var orderIntent = await _orderService.CreateOrderIntent(placeOrderDto);
+            if (orderIntent == null)
+            {
+                return BadRequest("Failed to create order. Please check your order details.");
+            }
+
+            return CreatedAtAction(nameof(CreateOrderForBranchManager), new { id = orderIntent.OrderIntentId }, orderIntent);
+        }
+        catch (Exception ex)
         {
-            return BadRequest("Failed to create order. Please check your order details.");
+            return StatusCode(500, $"An error occurred while processing your request: {ex.Message}");
         }
-
-        return CreatedAtAction(nameof(CreateOrderForBranchManager), new { id = orderIntent.OrderIntentId }, orderIntent);
     }
-    catch (Exception ex)
-    {
-        return StatusCode(500, $"An error occurred while processing your request: {ex.Message}");
-    }
-}
 }
