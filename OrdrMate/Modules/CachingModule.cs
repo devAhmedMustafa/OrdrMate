cf383fd4499351b741080b6e54455a2cff1f4d6e
Ahmed Mustafa <am1804067@gmail.com>
2025-10-04 08:23:09 +0300

    Redis caching Module

---
+++ OrdrMate/Modules/CachingModule.cs
@@ -0,0 +1,29 @@
+using MongoDB.Driver;
+using OrdrMate.Core;
+using StackExchange.Redis;
+
+namespace OrdrMate.Modules;
+
+public class CachingModule : IModule
+{
+
+    public void Register(
+        IServiceCollection services,
+        IConfiguration configuration,
+        IHostEnvironment environment
+    )
+    {
+        services.AddSingleton<IConnectionMultiplexer>(sp =>
+        {
+            var redisConfig = configuration.GetConnectionString("Redis");
+
+            if (string.IsNullOrEmpty(redisConfig))
+            {
+                throw new InvalidOperationException("Redis connection string is not configured.");
+            }
+
+            return ConnectionMultiplexer.Connect(redisConfig);
+        });
+    }
+
+}
